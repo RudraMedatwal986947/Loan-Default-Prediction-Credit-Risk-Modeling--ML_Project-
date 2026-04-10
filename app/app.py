e3aa21a8a9b5cb05a6dae31e15300a6c78c5c0f6
RudraMedatwal986947 <106727969+RudraMedatwal986947@users.noreply.github.com>
2026-04-10 17:06:26 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ import joblib
 import pandas as pd
 
 model = joblib.load("models/loan_model.joblib")
-
+st.write(type(model))
 st.title("Loan Default Prediction System")
 st.write("Model Used: Logistic Regression (Optimized for Recall)")
 
